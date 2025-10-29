5d9b80692c801b8a9ab7bf2889f40c49298668fd
azui-huz <132163636+azui-huz@users.noreply.github.com>
2025-10-29 20:34:29 +0000

    Cleaning doxygen

--- src/main.c
+++ src/main.c
@@ -1,15 +1,6 @@
-/**
- * @file main.c
- * @brief Entry point for the state machine program.
- */
-
 #include "machine.h"
 #include <stdio.h>
 
-/**
- * @brief Program entry point.
- * Initializes the machine and starts the execution loop.
- */
 int main() {
     Machine machine;
     initialize_machine(&machine, "main");
